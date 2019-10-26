16805029b8669cd6060011e01e0750d1a95b3ee7
Valterfi Oliveira <valterfi.oliveira@gmail.com>
2019-10-26 16:41:39 -0700

    improving selecting element for scroliing smoth

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ document.addEventListener("DOMContentLoaded", function() {
         // add click listener for the element in this iteration of the loop
         element.addEventListener('click', function(event) {
             event.preventDefault();
-            // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
-            let hrefString = element.href;
 
+            // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
+            //let hrefString = element.href;
             // get the section by its id using the string from the last step
-            let section = hrefString.split('#')[1];
+            let section = element.hash;
 
             // scroll to that section with .scrollIntoView()
-            document.querySelector("#" + section).scrollIntoView({ 
+            document.querySelector(section).scrollIntoView({ 
                 behavior: 'smooth' 
             });
 
