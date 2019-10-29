9c4d6c24c94717ce1d056f59cd83a7b9dc2cd26e
Valterfi Oliveira <valterfi.oliveira@gmail.com>
2019-10-29 14:06:18 -0700

    some check improvements

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
 
     const navLinks = document.querySelectorAll('.nav-item');
 
     navLinks.forEach(function(element) {
         // add click listener for the element in this iteration of the loop
-        element.addEventListener('click', function(event) {
+        $(element).click(function(event) {
             event.preventDefault();
 
             // how can you get the "href" attribute value of the clicked element to create a string that corresponds the id of the correct section?
